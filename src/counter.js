9d193b4fc572f295c647ae0cffd1d77bd9f13ce6
racerf01 <racerf01@mail.ru>
2024-02-08 00:32:30 +0100

    lesson 2

---
+++ src/counter.js
@@ -0,0 +1,34 @@
+import { Component } from "react";
+import React from "react";
+import { ThemeContext } from "./App";
+
+export default class Counter extends Component {
+constructor(props) {
+    super(props)
+
+    this.state = {
+        count: props.initialCount
+    }
+}
+
+    render() {
+        console.log('Render App')
+        return ( 
+            <ThemeContext.Consumer>
+                {style => ( 
+                <div>
+                    <button style={style} onClick={() => this.changeCount(-1)}>-</button>
+                    <span>{this.state.count}</span>
+                    <button style={style} onClick={() => this.changeCount(1)}>+</button>
+                </div>
+                )}
+            </ThemeContext.Consumer>
+        )
+    }
+
+    changeCount(amount) {
+        this.setState(prevState => {
+            return {count: prevState.count + amount}
+        })
+    }
+}
